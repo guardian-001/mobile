ae922378b4e8877ef1212dfc4a96563dc1ae69ef
AzizSandid <aziz.stage@gmail.com>
2024-07-02 16:01:21 +0100

    feat(components): decompose checkbox file into three files

--- src/app/(architect)/(private)/index.tsx
+++ src/app/(architect)/(private)/index.tsx
@@ -1,21 +1,17 @@
 import { FlashList } from '@shopify/flash-list';
-import React, { useState } from 'react';
+import React from 'react';
 
 import type { Post } from '@/api';
 import { usePosts } from '@/api';
 import {
-  Checkbox,
   EmptyList,
   FocusAwareStatusBar,
-  Radio,
-  Switch,
   Text,
   View,
 } from '@/shared/components';
 import { Card } from '@/shared/utils/card';
 
 export default function Feed() {
-  const [checked, setChecked] = useState(false);
   const { data, isPending, isError } = usePosts();
   const renderItem = React.useCallback(
     ({ item }: { item: Post }) => <Card {...item} />,
@@ -30,29 +26,8 @@ export default function Feed() {
     );
   }
   return (
-    <View className="flex-1 ">
+    <View className="bg-primary flex-1">
       <FocusAwareStatusBar />
-      <Checkbox
-        checked={checked}
-        onChange={setChecked}
-        label="aaa"
-        accessibilityLabel="aza"
-        className="m-8"
-      />
-      <Switch
-        checked={checked}
-        onChange={setChecked}
-        label="aaa"
-        accessibilityLabel="aza"
-        className="m-8"
-      />
-      <Radio
-        checked={checked}
-        onChange={setChecked}
-        label="aaa"
-        accessibilityLabel="aza"
-        className="m-8"
-      />
       <FlashList
         data={data}
         renderItem={renderItem}
