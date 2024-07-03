502d6db17d0e3d395ee0cfce9b86e3be50980db7
“elayeboussama” <oussama.archimatch@gmail.com>
2024-07-03 15:03:28 +0100

    feat(start screen): add shared  screen and fix husky errors

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,17 +1,15 @@
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': (filenames) => [
-    `npx eslint --fix --ignore-path .eslintignore ${filenames
-      .map((filename) => `"${filename}"`)
-      .join(' ')}`,
-    `npx tsc --noEmit --skipLibCheck --project tsconfig.json`,
-  ],
-  '**/*.(md|json)': (filenames) => [
-    `npx prettier --write --ignore-path .prettierignore ${filenames
+    `npx eslint --fix ${filenames
       .map((filename) => `"${filename}"`)
       .join(' ')}`,
   ],
+  '**/*.(md|json)': (filenames) =>
+    `npx prettier --write ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')}`,
   'src/translations/*.(json)': (filenames) => [
-    `npx eslint --fix --ignore-path .eslintignore ${filenames
+    `npx eslint --fix ${filenames
       .map((filename) => `"${filename}"`)
       .join(' ')}`,
   ],
