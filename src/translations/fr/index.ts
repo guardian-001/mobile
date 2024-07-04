e9d4ae3fe2020f74d1fc27636f5fe4cc23ccde90
“elayeboussama” <oussama.archimatch@gmail.com>
2024-07-04 10:43:08 +0100

    fix(errors): fixed errors for translation folder and main button component

--- src/translations/fr/index.ts
+++ src/translations/fr/index.ts
@@ -1,6 +1,6 @@
 import common from './common.json';
 import login from './login.json';
-import onBoarding from './onboarding.json';
+import onBoarding from './onBoarding.json';
 import resetpass from './resetpass.json';
 import settings from './settings.json';
 import signupStep1 from './signup-step1.json';
