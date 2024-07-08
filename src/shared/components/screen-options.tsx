44b63038da1d5af694b41c8cb1c5e430fa05561d
guardian-001 <oussama.archimatch@gmail.com>
2024-07-08 15:40:03 +0100

    Create screen-options.tsx

---
+++ src/shared/components/screen-options.tsx
@@ -0,0 +1,13 @@
+export const loginScreenOptions = {
+  headerTransparent: true,
+  headerTitle: '',
+  headerShown: true,
+  headerLeft: () => <BackButton route={'init'} />,
+};
+
+export const resetPasswordScreenOptions = {
+  headerTransparent: true,
+  headerTitle: '',
+  headerShown: true,
+  headerLeft: () => <BackButton />,
+};
