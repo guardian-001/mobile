158bca4c11b437b1ca9e06d74c1414efbad72374
AzizSandid <aziz.stage@gmail.com>
2024-07-23 09:45:08 +0100

    feat(project): create interface and add schema validation to step 7

--- src/modules/client/create-announcement/steps/save-project.tsx
+++ src/modules/client/create-announcement/steps/save-project.tsx
@@ -5,11 +5,74 @@ import { translate } from '@/core';
 import { StepperButton } from '@/modules/shared';
 import { View } from '@/shared/components';
 import type { StepperFormProps } from '@/types';
-export function SaveProject({}: StepperFormProps) {
+/*import { AnnouncementType } from '@/types/announcement';
+import { CreateAnnouncementStepTwelveSchema } from '../schemas';
+import { ControlledPhoneNumberInput } from '@/shared/components/controlled-phone-number-input';*/
+export function SaveProject({}: // onHandleBack,
+//onHandleNext,
+//setFormData,
+//formData,
+StepperFormProps) {
   const router = useRouter();
+  /*const { handleSubmit, control, errors } = useCustomForm(
+    CreateAnnouncementStepTwelveSchema,
+    { firstName: formData?.firstName,
+      lastName: formData?.lastName,
+      email: formData?.email,
+      phoneNumber: formData?.phoneNumber, 
+      rules: formData?.rules,
+      receiveNotifications: formData?.receiveNotifications
+    }
+  );
+  type CreateProfileFormType = Pick<AnnouncementType, 
+  | 'firstName'
+  | 'lastName'
+  | 'email'
+  | 'phoneNumber'
+  | 'rules'
+  | 'receiveNotifications'
+    >;
+
+  const onSubmit = (data: CreateProfileFormType) => {
+    setFormData((prev: any) => ({
+      ...prev,
+      ...data,
+    }));
+    onHandleNext();
+  };
+*/
   return (
     <View className="flex flex-1 items-center justify-between pt-8">
-      <View className="h-[85%]" />
+      {/*     <View className=" flex h-fit w-[90%] gap-5 rounded-3xl bg-white px-4 py-5 shadow-md">
+        <ControlledInput
+          testID="name-input"
+          control={control}
+          name="firstName"
+          label={translate('labels.name')}
+          placeholder={translate('labels.name')}
+        />
+        <ControlledInput
+          testID="surname-input"
+          control={control}
+          name="lastName"
+          label={translate('labels.surname')}
+          placeholder={translate('labels.surname')}
+        />
+        <ControlledInput
+          testID="email-input"
+          control={control}
+          name="email"
+          label={translate('labels.mail')}
+          placeholder={translate('labels.mail')}
+        />
+        <ControlledPhoneNumberInput
+          name="phoneNumber"
+          control={control}
+          label={translate('labels.phone')}
+          rules={{ required: 'Phone number is required' }}
+        />
+  
+      </View>*/}
       <StepperButton
         onPressHandler={() => {
           router.back();
