6044b17ca22e85642764a64c6224f50c65bfa694
AzizSandid <aziz.stage@gmail.com>
2024-07-26 12:56:36 +0100

    feat(api): api

--- src/modules/client/create-announcement/steps/choose-speciality.tsx
+++ src/modules/client/create-announcement/steps/choose-speciality.tsx
@@ -3,36 +3,52 @@ import React from 'react';
 import { useStylesApi } from '@/api/client';
 import { translate } from '@/core';
 import { StepperButton } from '@/modules/shared';
-import { Text, ToggleCard, View } from '@/shared/components';
+import {
+  EmptyList,
+  ErrorData,
+  Text,
+  ToggleCard,
+  View,
+} from '@/shared/components';
 
 import { useSpeciality } from '../hooks';
 
 export function ChooseSpeciality() {
   const { handleSubmit, control, error, onSubmit } = useSpeciality();
-  const { data: SpecialityData } = useStylesApi();
+  const { data: SpecialityData, isError, isLoading } = useStylesApi();
   return (
-    <View className="flex flex-1 justify-between pt-4">
-      <View className="h-[85%] items-center gap-4">
-        {SpecialityData?.map((cardData, index) => (
-          <ToggleCard
-            key={index}
-            className="h-56 w-60 rounded-2xl"
-            title={cardData.label}
-            image={cardData.icon}
-            name="architectSpeciality"
-            control={control}
-            value={cardData.id}
-            classNameText="my-3"
+    <View className="flex-1 pt-4">
+      {isError ? (
+        <ErrorData message="Error Loading Data" />
+      ) : (
+        <View className="justify-betwee flex flex-1">
+          {isLoading || SpecialityData?.length === 0 ? (
+            <EmptyList isLoading={isLoading} />
+          ) : (
+            <View className="h-[85%] items-center gap-4">
+              {SpecialityData?.map((cardData, index) => (
+                <ToggleCard
+                  key={index}
+                  className="h-56 w-60 rounded-2xl"
+                  title={cardData.label}
+                  image={cardData.icon}
+                  name="architectSpeciality"
+                  control={control}
+                  value={cardData.id}
+                  classNameText="my-3"
+                />
+              ))}
+            </View>
+          )}
+          {error && (
+            <Text className="text-sm text-error dark:text-error" tx={error} />
+          )}
+          <StepperButton
+            onPressHandler={handleSubmit(onSubmit)}
+            label={translate('signup.suivant')}
           />
-        ))}
-      </View>
-      {error && (
-        <Text className="text-sm text-error dark:text-error" tx={error} />
+        </View>
       )}
-      <StepperButton
-        onPressHandler={handleSubmit(onSubmit)}
-        label={translate('signup.suivant')}
-      />
     </View>
   );
 }
