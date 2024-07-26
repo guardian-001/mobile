8f32c27af48a9a020a81ad5ac9301edb5ab41061
AzizSandid <aziz.stage@gmail.com>
2024-07-26 12:31:18 +0100

    feat(api): api

--- src/services/shared/announcement-service.ts
+++ src/services/shared/announcement-service.ts
@@ -1,52 +1,48 @@
-import { Env } from '@env';
 import type { AxiosError } from 'axios';
-import axios from 'axios';
 
+import { client } from '@/api';
 import type { resultType } from '@/api/client';
-import { showError } from '@/shared/components';
 
 function isAxiosError(error: unknown): error is AxiosError {
   return (error as AxiosError).isAxiosError !== undefined;
 }
 
 export async function getAnnouncementStep1(): Promise<resultType[]> {
-  try {
-    const url = `${Env.API_URL}/api/announcement/architect-specialities`;
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error: unknown) {
-    if (isAxiosError(error)) {
-      showError(error);
-      throw new Error(
-        `API request failed with status ${error.response?.status}`
-      );
-    } else {
-      throw new Error(
-        `API request failed: ${
-          error instanceof Error ? error.message : 'Unknown error'
-        }`
-      );
-    }
-  }
+  const url = `/api/announcement/architect-specialities`;
+  return client
+    .get(url)
+    .then((response) => response.data)
+    .catch((error: unknown) => {
+      if (isAxiosError(error)) {
+        throw new Error(
+          `API request failed with status ${error.response?.status}`
+        );
+      } else {
+        throw new Error(
+          `API request failed: ${
+            error instanceof Error ? error.message : 'Unknown error'
+          }`
+        );
+      }
+    });
 }
 
 export async function getAnnouncementStep3(): Promise<resultType[]> {
-  try {
-    const url = `${Env.API_URL}/api/announcement/project-categories`;
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error: unknown) {
-    if (isAxiosError(error)) {
-      showError(error);
-      throw new Error(
-        `API request failed with status ${error.response?.status}`
-      );
-    } else {
-      throw new Error(
-        `API request failed: ${
-          error instanceof Error ? error.message : 'Unknown error'
-        }`
-      );
-    }
-  }
+  const url = `/api/announcement/project-categories`;
+  return client
+    .get(url)
+    .then((response) => response.data)
+    .catch((error: unknown) => {
+      if (isAxiosError(error)) {
+        throw new Error(
+          `API request failed with status ${error.response?.status}`
+        );
+      } else {
+        throw new Error(
+          `API request failed: ${
+            error instanceof Error ? error.message : 'Unknown error'
+          }`
+        );
+      }
+    });
 }
