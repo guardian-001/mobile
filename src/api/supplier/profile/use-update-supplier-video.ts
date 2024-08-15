f5d388932b2b5b9297b5788e198df0574bd2536b
“elayeboussama” <oussama.archimatch@gmail.com>
2024-08-15 14:35:37 +0100

    feat(supplier profile): video uploader

--- src/api/supplier/profile/use-update-supplier-video.ts
+++ src/api/supplier/profile/use-update-supplier-video.ts
@@ -8,6 +8,6 @@ export const useUpdateSupplierVideoApi = createMutation<
   Request,
   AxiosError
 >({
-  mutationKey: ['updateSupplierBio'],
+  mutationKey: ['updateSupplierVideo'],
   mutationFn: updateSupplierVideoAsync,
 });
