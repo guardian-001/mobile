083f86e5bf5fa92bab2c847710fd7dc36b2d01b5
AzizSandid <aziz.stage@gmail.com>
2024-08-15 12:05:21 +0100

    feat(supplier): add Subscription

--- src/modules/supplier/profile/subscription-management.tsx
+++ src/modules/supplier/profile/subscription-management.tsx
@@ -1,11 +1,31 @@
+import { useRouter } from 'expo-router';
 import React from 'react';
 
-import { Text, View } from '@/shared/components';
+import { translate } from '@/core';
+import { Button, Image, Text, View } from '@/shared/components';
 
 export const SubscriptionManagement = () => {
+  const router = useRouter();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Subscription management</Text>
+    <View className="flex-1">
+      <Image
+        source={require('@/assets/images/subscription-image.png')}
+        className="h-72 w-full"
+      />
+      <Text
+        tx="supplierProfile.upgradeToPremium"
+        className="-mt-8 text-center text-3xl font-extrabold"
+      />
+      <View className="flex-1 p-6 pb-8">
+        <View className="flex-1" />
+        <Button
+          label={translate('common.subscribe')}
+          onPress={() => {
+            router.back();
+          }}
+          className="h-12 rounded-lg"
+        />
+      </View>
     </View>
   );
 };
