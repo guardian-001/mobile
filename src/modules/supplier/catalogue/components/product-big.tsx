6868ea59220acad2767d2da75f0c8eb5a30ec24b
“elayeboussama” <oussama.archimatch@gmail.com>
2024-09-12 18:56:31 +0100

    fix(big product view): height and component mal-rendering

--- src/modules/supplier/catalogue/components/product-big.tsx
+++ src/modules/supplier/catalogue/components/product-big.tsx
@@ -8,8 +8,8 @@ import { Button, colors, Modal, Text } from '@/shared/components';
 
 import type { Product } from '../../profile/type';
 import { useDeleteProduct } from '../hooks/use-delete-product';
+import BigImageSlider from './big-image-slider';
 import DeleteProductForm from './confirm-delete-product';
-import ImageSlider from './image-slider';
 
 type ProductBigProps = {
   item: Product;
@@ -33,7 +33,7 @@ const ProductBig = ({ item, index, onAddProductPress }: ProductBigProps) => {
 
   return (
     <View className="m-2   flex-1 items-center rounded-3xl bg-white pl-4 pt-4 shadow-md shadow-gray-300">
-      <ImageSlider images={item.productImages.map((img) => img.image)} />
+      <BigImageSlider images={item.productImages.map((img) => img.image)} />
       <View className="items-between w-full flex-1 flex-row justify-between">
         <View>
           <Text className="mb-1 text-lg font-bold">{item.name}</Text>
