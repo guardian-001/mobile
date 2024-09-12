fc1492e35b73e27f92afd975709f5836141dd16d
“elayeboussama” <oussama.archimatch@gmail.com>
2024-09-12 20:14:45 +0100

    feat(supplier): search drop list

--- src/modules/supplier/catalogue/components/products-little-grid.tsx
+++ src/modules/supplier/catalogue/components/products-little-grid.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, Pressable, TouchableOpacity, View } from 'react-native';
 
 import { Search } from '@/assets/icons';
 import { translate } from '@/core';
-import { ControlledInput } from '@/shared/components';
+import { ControlledInput, Text } from '@/shared/components';
 
 import type { Product } from '../../profile/type';
 import { DEFAULT_PRODUCT } from '../../shared/constant';
@@ -16,17 +16,50 @@ type ProductGridProps = {
 };
 
 const ProductLittleGrid = ({ items, onAddProductPress }: ProductGridProps) => {
-  const { control, filteredItems } = useProductManagement(items);
+  const {
+    control,
+    filteredItems,
+    selectedValue,
+    searchValue,
+    handleSelectItem,
+    listState,
+    setListState,
+  } = useProductManagement(items);
 
   return (
     <View className="mt-2 flex gap-2">
-      <View className="px-5">
+      <View className="  absolute z-10 w-full px-5">
         <ControlledInput
           icon={<Search />}
           control={control}
           name="search"
           placeholder={translate('labels.search')}
+          choiceValue={selectedValue !== null ? selectedValue : undefined}
+          handleOnChange={() => setListState(true)}
         />
+        {searchValue && listState && (
+          <Pressable
+            onPress={() => setListState(false)}
+            className="  h-96 w-full rounded-lg "
+          >
+            <View className=" mb-1  w-full rounded-lg  bg-white">
+              <FlatList
+                data={filteredItems}
+                renderItem={({ item }: { item: Product }) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      handleSelectItem(item.name);
+                    }}
+                  >
+                    <Text className="text-lg font-bold">{`* ${item.name}`}</Text>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerClassName="px-4 py-4"
+              />
+            </View>
+          </Pressable>
+        )}
       </View>
       <FlatList
         data={[DEFAULT_PRODUCT, ...filteredItems]}
@@ -40,7 +73,7 @@ const ProductLittleGrid = ({ items, onAddProductPress }: ProductGridProps) => {
         keyExtractor={(item) => item.id.toString()}
         numColumns={2}
         columnWrapperClassName="justify-between"
-        contentContainerClassName="px-2 pt-2 pb-24"
+        contentContainerClassName="px-2 pt-2 pb-24 mt-12"
       />
     </View>
   );
