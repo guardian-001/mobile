ff4316bb236e4d4aaa22345823f793508003e4e9
“elayeboussama” <oussama.archimatch@gmail.com>
2024-09-24 04:58:37 +0100

    fix(app): folder structure

--- src/app/(architect)/(private)/(tab)/profile.tsx
+++ src/app/(architect)/(private)/(tab)/profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ArchitectProfile from '@/modules/architect/profile/profile/architect-profile';
+import ArchitectProfile from '@/modules/architect/profile-tab/architect-profile';
 import { FocusAwareStatusBar, View } from '@/shared/components';
 
 export default function Profile() {
